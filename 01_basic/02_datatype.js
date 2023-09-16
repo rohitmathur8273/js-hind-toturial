d5f2e271a87d88a5e142c7a084ce20c6d0f09b32
shyam sundar <79298661+rohitmathur8273@users.noreply.github.com>
2023-09-16 14:21:25 +0530

    datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all /js code newer version 
+
+// number => 2 to power 53 
+// bigint
+// string => "rohit"
+// boolean => true // false
+// null=> standalone value
+//undefined=>
+// symbol=> unique
