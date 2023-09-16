72b6b9cab771d39cd4192e30a62bddd94356cb89
shyam sundar <79298661+rohitmathur8273@users.noreply.github.com>
2023-09-16 10:33:32 +0000

    datatype summary

---
+++ 01_basic/04_datatype_summary.js
@@ -0,0 +1,30 @@
+// primitive
+
+// 7 type : String, Number , Boolen , null , undefined, symbol ,BigInt 
+
+
+// consts score = 100 
+ // const scoreValue = 100.3
+ //  const isLoggedIn = false
+ // outsideTemp = null 
+ // let userEmail;
+
+ // const id = Symbol('123')
+ // const anotherId = Symbol('123') // console.log(id === anotherId ) is not equal too 
+
+// Reference type ( Non primitive )
+
+// Array , Objects , Functions
+
+// const heros = ["shaktimaan",  "mosa" , "doga"]
+
+// {
+//      name: "rohit",
+//      age:23,
+//      email: "rohitmathur8273@gmial.com"
+// }
+
+// // function 
+// // const myFunction = function (){
+//     console.log("hello code")
+// }
