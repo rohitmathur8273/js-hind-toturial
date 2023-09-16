b878bf18b8fc54b732fdbff9dc042c20591a0caf
shyam sundar <79298661+rohitmathur8273@users.noreply.github.com>
2023-09-16 10:02:12 +0000

    datatype_conversion

---
+++ 01_basic/03_datatype_conversion.js
@@ -0,0 +1,15 @@
+let score = "333ghjk"
+
+console.log(typeof score);
+
+//how to change string  to number 
+ let valueInNumber = Number(score )
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+
+
+ // "33" => 33
+ //"33"=> NaN
+ // true => 1: false =>  0  
+
+ ///*********************operation********************
